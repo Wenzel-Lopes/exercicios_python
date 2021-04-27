a5a5b12a60105f4d6d4b06c340d2dd581ba1cc44
Wenzel-Lopes <80937608+Wenzel-Lopes@users.noreply.github.com>
2021-04-26 23:36:34 -0300

    Update transacoes.py

--- Exercicios-com-loops/transacoes.py
+++ Exercicios-com-loops/transacoes.py
@@ -4,18 +4,18 @@ print("*****************************************")
 
 input("Pressione a tecla \'ENTER\' para iniciarmos: ")
 
-transações = int(input("\nDigite quantas transações financeiras você realizou ao longo de um dia: "))
+transacoes = int(input("\nDigite quantas transações financeiras você realizou ao longo de um dia: "))
 
 i = 1
 total = 0
 soma = 0
 
-while i <= transações:
+while i <= transacoes:
     soma = float(input("\nDigite o valor da {}º transação realizada: ".format(i)))
     total = total + soma
     i = i +1
 
-media = total/5
+media = total/transacoes
 
-print("\nVocê realizou {} tranzações, o total gasto foi R${:.2f}, e a sua média de gasto é de R${:.2f}".format(transações, total, media))
+print("\nVocê realizou {} tranzações, o total gasto foi R${:.2f}, e a sua média de gasto é de R${:.2f}".format(transacoes, total, media))
 
